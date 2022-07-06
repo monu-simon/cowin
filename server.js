d5f629380d30d148f382676247d14357898b39c2
monu-simon <monusimonm@gmail.com>
2022-07-06 20:45:58 +0530

    Deployment to heroku changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/app'));
-app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/app/index.html')); });
+app.use(express.static(__dirname + '/dist/cowin'));
+app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/cowin/index.html')); });
 
 app.listen(process.env.PORT || 8080);
